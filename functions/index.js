233cfbb92debc05a195f895ad8befc15da529785
Diana Tineo <diana.tineo1@gmail.com>
2021-11-12 17:36:36 -0500

    added all endpoints for event

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
 const functions = require("firebase-functions");
 const express = require('express')
 const cors = require('cors');
-const { getEvents } = require("./src/events");
+const { getEvents, createEvent, updateEvent, deleteEvent, getEventById } = require("./src/events");
 const app = express()
 app.use = (cors())
 
 app.get('/events', getEvents)
-// app.get('/events',)
-// app.post('/events',)
+app.get('/events/:eventId', getEventById)
+app.post('/events', createEvent)
+app.patch('/events/:eventId', updateEvent)
+app.delete('/events', deleteEvent)
+
+// app.get('/places', getPlaces)
+// app.get('/places/:placeId', getPlaceById)
+// app.post('/places', createPlace)
+// app.patch('/places/:placesId', updatePlace)
+// app.delete('/places/:placeId', deleteEvent)
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 exports.app = functions.https.onRequest(app)
--- functions/src/events.js
+++ functions/src/events.js
@@ -1,4 +1,4 @@
-const { connectDb } = require("./db")
+const { connectDb } = require("./connection/db")
 
 exports.getEvents = async (req, res) =>{
   const db = connectDb()
@@ -12,4 +12,70 @@ exports.getEvents = async (req, res) =>{
     res.status(200).send(events)
   })
   .catch(err => res.status(500).send(err))
+}
+
+
+exports.createEvent = (req, res) => {
+  //check that all required fields are present
+  if(!req.body.name || !req.body.location || !req.body.date || !req.body.time){
+    res.status(401).send({ message: 'Invalid Input'})
+    return //exits the function
+  }
+
+  const date = new Date(req.body.date)
+  //construct new clothing from .body
+  let newEvent = {
+    name: req.body.name,
+    type: req.body.type,
+    date: date, 
+    time: req.body.time,
+    location: req.body.location,
+    organizer: req.body.organizer
+  }
+  
+  const db = connectDb()
+    db.collection('events').add(newEvent)
+    .then(docRef => res.status(201).send({ id: docRef.id }))
+    .catch(err => res.status(201).send(err))
+  
+}
+
+exports.getEventById =(req, res) => {
+  //connect to database
+  const db = connectDb()
+  //get eventId from the req.param
+  //const eventId = req.params.productId
+  const { eventId } = req.params
+  //get document from collection
+  db.collection('events').doc(eventId).get()
+  .then(doc => {
+    let event = doc.data()
+    event.id = doc.id
+    res.send(event)
+  })
+  .catch(err => res.status(500).send(err))
+  //return document
+}
+
+exports.updateEvent = (req, res) => {
+  const { eventId } = req.params
+  let newUpdates = {}
+  if(req.body.type) newUpdates.type = req.body.type
+  if(req.body.name) newUpdates.name = req.body.name
+  if(req.body.date) newUpdates.date = req.body.date 
+  if(req.body.location) newUpdates.location = req.body.location
+  if(req.body.organizer) newUpdates.organizer = req.body.organizer
+  
+  const db = connectDb()
+  db.collection('events').doc(eventId).update(req.body)
+  .then(() => res.status(201).send({message: 'updated'}))
+  .catch(err => res.status(500).send(err))
+}
+
+exports.deleteEvent = (req, res) => {
+  const { eventId } = req.params
+  const db = connectDb()
+  db.collection('events').doc(eventId).delete()
+    .then(()=> res.status(202).send({message: 'deleted'}))
+    .catch(err => res.status(500).send(err))
 }
